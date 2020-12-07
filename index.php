b8a3ad9e55847bb01ca7be335cb2a1f4342df07b
Fijo-Lurk <filip.johansson@chasacademy.se>
2020-12-07 18:36:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>To-do-list</title>
+</head>
+<body>
+
+   <header class="todo-description">
+    <h1>Make your own to-do-list</h1>
+    <h1>Assignments</h1>
+   </header>
+
+
+
+<section class="main-section">
+<div class="add-section">
+<form action="">
+    <input
+    class="to-do-input"
+    type="text"
+    name="title"
+    placeholder="This field is required" id="">
+    <button class="submit-btn" type="submit">Add To-do</button>
+</form>
+</div>
+</section>
+
+</body>
+</html>
